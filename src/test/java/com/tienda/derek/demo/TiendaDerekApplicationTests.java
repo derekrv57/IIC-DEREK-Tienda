02e3587347f91eae38fc5a9dc4c85375d5d30fc6
Derek Rodríguez <derekrv57@gmail.com>
2023-09-27 17:21:31 -0600

    Cambios semana 2
    Tarea aside y section

---
+++ src/test/java/com/tienda/derek/demo/TiendaDerekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda.derek.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaDerekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
