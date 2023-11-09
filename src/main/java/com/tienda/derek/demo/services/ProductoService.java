a951424ac75989da726a807901af96527761a31e
Derek Rodríguez <derekrv57@gmail.com>
2023-11-08 20:41:58 -0600

    Semana 9

--- src/main/java/com/tienda/derek/demo/services/ProductoService.java
+++ src/main/java/com/tienda/derek/demo/services/ProductoService.java
@@ -12,15 +12,18 @@ import java.util.List;
  * @author derek
  */
 public interface ProductoService {
-    public List<Producto> getProductos(boolean activos);
-    
+    public List<Producto> getProductos(boolean activo);
     // Se obtiene un Producto, a partir del id de un producto
     public Producto getProducto(Producto producto);
-    
     // Se inserta un nuevo producto si el id del producto esta vacío
     // Se actualiza un producto si el id del producto NO esta vacío
     public void save(Producto producto);
-    
     // Se elimina el producto que tiene el id pasado por parámetro
     public void delete(Producto producto);
+    // Lista de productos con precio entre ordendados por descripción ConsultaAmpliada
+    public List<Producto> findByPrecioBetweenOrderByDescripcion(double precioInf, double precioSup);
+    //Lista de productos utilizando consultas con JPQL    
+    public List<Producto> metodoJPQL(double precioInf, double precioSup);
+    //Lista de productos utilizando consultas con SQL Nativo
+    public List<Producto> metodoNativo(double precioInf, double precioSup);
 }
--- src/main/java/com/tienda/derek/demo/services/impl/ProductoServideImpl.java
+++ src/main/java/com/tienda/derek/demo/services/impl/ProductoServideImpl.java
@@ -14,31 +14,44 @@ public class ProductoServideImpl implements ProductoService {
     @Autowired
     private ProductoDao productoDao;
     @Override
-    @Transactional(readOnly=true)
+    @Transactional(readOnly = true)
     public List<Producto> getProductos(boolean activos) {
-        var lista=productoDao.findAll();
+        var lista = productoDao.findAll();
         if (activos) {
             lista.removeIf(e -> !e.isActivo());
         }
         return lista;
     }
-
     @Override
-    @Transactional
+    @Transactional(readOnly = true)
     public Producto getProducto(Producto producto) {
         return productoDao.findById(producto.getIdProducto()).orElse(null);
     }
-
     @Override
     @Transactional
     public void save(Producto producto) {
         productoDao.save(producto);
     }
-
     @Override
     @Transactional
     public void delete(Producto producto) {
         productoDao.delete(producto);
+    }
+    // Lista de productos con precio entre ordendados por descripción ConsultaAmpliada
+    @Override
+    @Transactional(readOnly = true)
+    public List<Producto> findByPrecioBetweenOrderByDescripcion(double precioInf, double precioSup) {
+        return productoDao.findByPrecioBetweenOrderByDescripcion(precioInf, precioSup);
+    }
+    @Override
+    @Transactional(readOnly = true)
+    public List<Producto> metodoJPQL(double precioInf, double precioSup) {
+        return productoDao.metodoJPQL(precioInf, precioSup);
+    }
+    @Override
+    @Transactional(readOnly = true)
+    public List<Producto> metodoNativo(double precioInf, double precioSup) {
+        return productoDao.metodoNativo(precioInf, precioSup);
     }
 
 }
